12ce62a95a56baca46da55b7cc4f8aab9d4ecede
Kunal Bhandari <kunal_nitk@yahoo.com>
2020-11-01 17:57:43 -0600

    Added better error handling

--- daily_stock_data.py
+++ daily_stock_data.py
@@ -13,13 +13,42 @@ from datetime import date
 
 ###################### Main program ############################
 # Following function to be run every day
-try:
-    load_daily_data_stooq()
-    load_daily_data_bse()
-    send_email(message = f"Stooq: Load daily data successful {date.today()}",
-              subject = f"daily stooq data {date.today()}")
-except:
-    send_email(message = f"Load daily data failed {date.today()}",
-              subject = f"daily stooq data {date.today()}")
-    raise
+failure_flag = False
+bad_sources = []
+bad_files = []
 
+try:
+    bad_files_stooq = load_daily_data_stooq()
+    
+    if len(bad_files_stooq) > 0:
+        bad_sources.append("Stooq")
+        bad_files = bad_files + bad_files_stooq
+        failure_flag = True
+except:
+    print(f"daily_stock_data.py: Error in load_daily_data_stooq")
+    bad_sources.append("Stooq")
+    failure_flag = True
+
+try:
+    bad_files_bse = load_daily_data_bse()
+    
+    if len(bad_files_bse) > 0:
+        bad_sources.append("BSE")
+        bad_files = bad_files + bad_files_stooq
+        failure_flag = True
+except:
+    print(f"daily_stock_data.py: Error in load_daily_data_bse")
+    bad_sources.append("BSE")
+    failure_flag = True
+    
+if failure_flag:
+    file_str = ""
+    if len(bad_files) > 0:
+        file_str = f"\nFailed in following files [{', '.join(bad_files)}]"
+    
+    send_email(message = f"daily_stock_data.py: Failed in load daily stocks for [{', '.join(bad_sources)}] on [{date.today()}]",
+              subject = f"Failed in load daily stocks for [{', '.join(bad_sources)}] on [{date.today()}]"
+               + file_str)
+else:
+    send_email(message = f"daily_stock_data.py: Load daily data successful {date.today()}",
+              subject = f"Success in daily stock data on [{date.today()}]")
