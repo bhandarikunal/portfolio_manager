d08391f940ff145fef7ff445166bee30fbaac6a9
Kunal Bhandari <kunal_nitk@yahoo.com>
2020-12-11 14:22:42 -0600

    Created separate functions for IN and US markets

---
+++ daily_stock_data_in.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+###################### Libraries ############################
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
+from common_py.bse.functions import *
+
+from datetime import date
+
+###################### Inputs ############################
+
+in_source = "bse"
+
+
+###################### Main program ############################
+# Following function to be run every day
+failure_flags = {}
+bad_sources = []
+bad_files = []
+
+source = in_source
+failure_flags[source] = False
+try:
+    if source == "bse":
+        bad_files_in = load_daily_data_bse()
+    else:
+        print(f"daily_stock_data_in.py: Invalid source for IN market data [{source}]")
+        failure_flags[source] = True
+    
+    if len(bad_files_in) > 0:
+        bad_sources.append(source)
+        bad_files = bad_files + bad_files_in
+        failure_flags[source] = True
+except:
+    print(f"daily_stock_data_in.py: Error in load_daily_data for IN market using source [{source}]")
+    bad_sources.append(source)
+    failure_flags[source] = True
+
+if np.any(list(failure_flags.values())):
+    file_str = ""
+    if len(bad_files) > 0:
+        file_str = "\n\nFailed in following files for IN market:\n" + '\n'.join(bad_files)
+    
+    msg = f"""daily_stock_data_in.py: Failed in load daily IN stocks for [{', '.join(bad_sources)}] on [{date.today()}]
+    {file_str}"""
+    
+    send_email(message = msg,
+              subject = f"Failed in load daily IN stocks for [{', '.join(bad_sources)}] on [{date.today()}]"
+               + file_str)
+else:
+    send_email(message = f"daily_stock_data_in.py: Load daily IN data successful {date.today()}",
+              subject = f"Success in daily IN stock data on [{date.today()}]")
+
---
+++ daily_stock_data_us.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+###################### Libraries ############################
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
+from common_py.stooq.functions import *
+from common_py.stock_analyzer import *
+from common_py.eoddata.functions import *
+
+from datetime import date
+
+###################### Inputs ############################
+
+us_meta_source = "nasdaq"
+
+#us_source = "stooq"
+us_source = "eoddata"
+
+
+###################### Main program ############################
+# Following function to be run every day
+failure_flags = {}
+bad_sources = []
+bad_files = []
+
+#Load US stocks' meta information
+source = us_meta_source
+try:
+    load_ticker_info(source=source)
+except:
+    print(f"daily_stock_data_us.py: Error in load_ticker_info for US market using source [{source}]")
+    bad_sources.append(source)
+    failure_flags[source] = True
+
+
+#Load US stocks' EOD ticker prices
+#source = "stooq"
+source = us_source
+failure_flags[source] = False
+try:
+    if source == "eoddata":
+        bad_files_us = load_daily_data_eoddata()
+    elif source == "stooq":
+        bad_files_us = load_daily_data_stooq()
+    else:
+        print(f"daily_stock_data_us.py: Invalid source for US market data [{source}]")
+        failure_flags[source] = True
+    
+    if len(bad_files_us) > 0:
+        bad_sources.append(source)
+        bad_files = bad_files + bad_files_us
+        failure_flags[source] = True
+except:
+    print(f"daily_stock_data_us.py: Error in load_daily_data for US market using source [{source}]")
+    bad_sources.append(source)
+    failure_flags[source] = True
+
+
+if np.any(list(failure_flags.values())):
+    file_str = ""
+    if len(bad_files) > 0:
+        file_str = "\n\nFailed in following files for US market:\n" + '\n'.join(bad_files)
+    
+    msg = f"""daily_stock_data_us.py: Failed in load daily stocks for [{', '.join(bad_sources)}] on [{date.today()}]
+    {file_str}"""
+    
+    send_email(message = msg,
+              subject = f"Failed in load daily stocks US for [{', '.join(bad_sources)}] on [{date.today()}]"
+               + file_str)
+else:
+    send_email(message = f"daily_stock_data_us.py: Load daily US data successful {date.today()}",
+              subject = f"Success in daily US stock data on [{date.today()}]")
+
+#Analyze and generate stock recommendations
+if not failure_flags[us_source]:
+    print(f"daily_stock_data_us.py: Calling stock analyzer for US stocks")
+    
+    select_tickers = get_ticker_recommendations(max_recommend=50)
+    send_email(message="List of recommended possibly value stocks:",subject="US Top Value Stocks",
+               df=select_tickers)        
