5c9c58ee0b3de65402bd7654705e3aa011eaf4aa
Kunal Bhandari <kunal_nitk@yahoo.com>
2021-10-11 18:35:52 -0500

    Download sec filings

---
+++ daily_filings_data_us.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+###################### Libraries ############################
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
+from common_py.sec.functions import *
+
+
+###################### Main program ############################
+
+print(f"download_sec_archive_files.py: Calling download_sec_archive_files")
+
+try:
+    download_missing_sec_archive_files()
+except Exception as e:
+    logger.exception(f"download_sec_archive_files.py: Error",
+                     exc_info=True)
+    send_email(message="", subject=f"Error download_sec_archive_files")
+send_email(message="", subject=f"Success in download_sec_archive_files")
